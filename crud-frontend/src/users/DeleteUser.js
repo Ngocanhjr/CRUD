b5000f224cc813aeada41c1b82e5b6b3ca5d7020
Lengocanh <Lengocanh3524@gmail.com>
2025-09-18 23:35:22 +0700

    add demo

---
+++ crud-frontend/src/users/DeleteUser.js
@@ -0,0 +1,18 @@
+// import axios from "axios";
+// import React from "react";
+// import { useParams } from "react-router-dom";
+
+// export default function DeleteUser() {
+//   const { id } = useParams();
+
+//   const loadUser = async () => {
+//     const result = await axios.get(`http://localhost:8080/user/${id}`);
+//     setUser(result.data);
+//   };
+
+//   const deleteUser = async (id) => {
+//     await axios.delete(`http://localhost:8080/user/${id}`);
+//     loadUser();
+//   };
+
+// }
